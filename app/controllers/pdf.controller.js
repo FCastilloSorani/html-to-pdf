e48c72e8ca902baf5e985b441080782a1ed4eced
FCastilloSorani <fercastillo.s.1997@gmail.com>
2022-08-01 11:18:54 -0300

    generate from url or html

--- app/controllers/pdf.controller.js
+++ app/controllers/pdf.controller.js
@@ -1,30 +1,60 @@
 const puppeteer = require('puppeteer');
 
-async function printPDF(html) {
+function headers(pdf) {
+  return {
+    'Content-Type': 'application/pdf',
+    'Content-Length': pdf.length,
+  };
+}
+
+async function printHtmlFromUrl(url) {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  await page.setContent(html, { waitUntil: 'networkidle0' });
+
+  await page.goto(url, { waitUntil: 'networkidle0' });
+
   const pdf = await page.pdf({ format: 'A4' });
 
   await browser.close();
+
+  return pdf;
+}
+
+async function printHtmlFromHtml(html) {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+
+  await page.setContent(html, { waitUntil: 'networkidle0' });
+
+  const pdf = await page.pdf({ format: 'A4' });
+
+  await browser.close();
+
   return pdf;
 }
 
 function getPdf(req, res) {
   const html = req.body;
+  const url = req.body.url;
 
-  if (!html) {
+  if (!html && !url) {
     res.status(400).send({
       message: 'Content can not be empty',
     });
+    return;
   }
 
-  printPDF(url).then((pdf) => {
-    res.set({
-      'Content-Type': 'application/pdf',
-      'Content-Length': pdf.length,
-    }),
+  if (url) {
+    printHtmlFromUrl(url).then((pdf) => {
+      res.set(headers(pdf));
       res.send(pdf);
+    });
+    return;
+  }
+
+  printHtmlFromHtml(html).then((pdf) => {
+    res.set(headers(pdf));
+    res.send(pdf);
   });
 }
 
--- app/routes/pdf.router.js
+++ app/routes/pdf.router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Middlewares
+router.use(express.json());
 router.use(express.text());
 
 // Controllers
